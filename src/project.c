3484d1ef89ff9bb00746c3ac891430c03b67fa13
Niqtan <niqban123@gmail.com>
2025-01-31 18:20:39 +0800

    Made new progress 01-31-2025

--- src/project.c
+++ src/project.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <SDL.h>
 #include <stdlib.h>
 
 
@@ -38,7 +39,10 @@ enum {
 char** grid;
 int i, j;
 
-main() {
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
+
+int main(int argc, char* args[]) {
     int min, max;
 
     printf("Welcome to...");
@@ -105,10 +109,39 @@ char *create_2d_array() {
 }
 
 //Rendering Function
-void render_grid(char **grid) {
+int render_grid(char **grid) {
     for (i = 0; i < row; i++) {
         for (j = 0; i < col; j++) {
+            SDL_Window* window = NULL;
+
+            SDL_Surface* screen_surface = NULL;
+
+            if (SDL_Init( SDL_INIT_VIDEO ) < 0 ) 
+            {
+                printf("SDL could not initialize: %s\n", SDL_GetError());
+            }
+            else {
+
+                window= SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+                
+                if (window == NULL) {
+                    printf("Windows Null bruh: %s\n" SDL_GetError());
+                }
+                else {
+                screen_surface = SDL_GetWindowSurface( window );
+
+                SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));
             
+                SDL_UpdateWindowSurface(window);
+
+                SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
+                }
+            }
+            SDL_DestroyWindow(window);
+
+            SDL_Quit();
+
+            return 0;
         }
     }
 }
